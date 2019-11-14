4a76670eaf7458d34389c6298aa26a757fdd6a28
KG <davidgjy@163.com>
2019-11-14 22:28:46 +0800

    project init

---
+++ source/spring-boot/origin-web/src/test/java/com/genesis/origin/web/OriginWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesis.origin.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
