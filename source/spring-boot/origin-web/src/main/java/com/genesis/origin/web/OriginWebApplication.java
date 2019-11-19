3a92cb549f824adfae99995f85bd9c2f5a8a56d9
Kelvin Gu <jgu55@its.jnj.com>
2019-11-19 16:24:01 +0800

    submit

--- source/spring-boot/origin-web/src/main/java/com/genesis/origin/web/OriginWebApplication.java
+++ source/spring-boot/origin-web/src/main/java/com/genesis/origin/web/OriginWebApplication.java
@@ -2,6 +2,7 @@ package com.genesis.origin.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author : KG
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * modified by :
  */
 @SpringBootApplication
+@EnableSwagger2
 public class OriginWebApplication {
 
 	public static void main(String[] args) {
